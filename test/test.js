dedcd1c05d57bb1566ac333886213e84e981a9c0
朱伯君 <zhubojun@GBDdeMacBook-Pro.local>
2017-08-29 19:55:16 +0800

    采用karma jasmine

--- test/test.js
+++ test/test.js
@@ -2,7 +2,6 @@
  * Created by zhubojun on 2017/7/28.
  */
 
-var expect = chai.expect;
 
 var Token_stream = new Token_stream();
 
@@ -13,13 +12,13 @@ var Token = new Token(8, 1);
 describe('Token', function () {
     describe('#getKind()', function () {
         it('it should return the kind', function () {
-            expect(Token.getKind()).to.be.equal(8);
+            expect(Token.getKind()).toBe(8);
         });
     });
 
     describe('#getVal()', function () {
         it('it should return the value', function () {
-            expect(Token.getVal()).to.be.equal(1);
+            expect(Token.getVal()).toBe(1);
         });
     });
 });
@@ -27,7 +26,7 @@ describe('Token', function () {
 describe('Token_stream', function () {
    describe('#getFull()', function () {
        it('it should return false', function () {
-           expect(Token_stream.full).to.be.equal(false);
+           expect(Token_stream.full).toBe(false);
        });
 
    });
@@ -35,41 +34,36 @@ describe('Token_stream', function () {
 
 
 describe('Calculate1', function () {
-    before(function () {
-        ac();
-        enter("1+2.33*5");
-    });
+
+    ac();
+    enter("1+2.33*5");
 
     describe('Calculate1', function() {
         it('it should return 12.65', function () {
-            expect(equal()).to.be.equal(12.65);
+            expect(equal()).toBe(12.65);
         });
     });
 });
 
 describe('Calculate2', function() {
-    before(function() {
-        ac();
-        enter("(1+2)*3");
-    });
 
     describe('Calculate2', function () {
         it('it should be 9', function () {
-            expect(equal().to.be.equal(9));
+            ac();
+            enter("(1+2)*3");
+            expect(equal().toBe(9));
         });
     });
 });
 
 describe('ac and ce', function () {
-    before(function (){
         ac();
         enter("(111+2)*9");
         ce();
-    });
 
     describe('Test ce', function () {
         it('it shold be (111+2)*', function() {
-            expect(getCharfromDisplay().to.be.equal("(111+2)*"));
+            expect(getCharfromDisplay().toBe("(111+2)*"));
         });
     });
 });
